a4ecc07620d0e1b8ae1cb20b0d3d562f59fa139b
RenFukatsu <fukatsuren88@gmail.com>
2019-08-14 15:26:32 +0900

    2019-08-14

--- ABC100/d.py
+++ ABC100/d.py
@@ -1,5 +1,4 @@
 import heapq
-import itertools
 
 N, M = map(int, input().split())
 
@@ -7,17 +6,16 @@ data = []
 for _ in range(N):
     data.append(list(map(int, input().split())))
 
+score = [] 
 scores = []
-for info in list(itertools.combinations(data, M)):
-    print('info', info)
-    beuty_score = abs(info[0][0] + info[1][0] + info[2][0])
-    taste_score = abs(info[0][1] + info[1][1] + info[2][1])
-    popular_score = abs(info[0][2] + info[1][2] + info[2][2])
-    print(beuty_score)
-    print(taste_score)
-    print(popular_score)
-    heapq.heappush(scores, -(beuty_score + taste_score + popular_score))
+for i in range(-1, 2, 2):
+    for j in range(-1, 2, 2):
+        for k in range(-1, 2, 2):
+            for info in data:
+                heapq.heappush(score, -(i*info[0] + j*info[1] + k*info[2]))
+            scores.append(-sum([heapq.heappop(score) for _ in range(M)]))
+            score.clear()
 
+print(max(scores))
 
-print(-heapq.heappop(scores))
-
+                
